65705401a4da11ceb831777ba3dd3e3f061bc3ca
Saksham4sure <sakshamorig123@gmail.com>
2025-11-02 16:24:52 +0545

    Swiper9

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <>
       <div className="overflow-x-hidden">
-        <div className="pt-24 md:pt-28 px-2 md:px-10 border flex flex-col items-center">
+        <div className="pt-24 md:pt-28 px-2 md:px-10 flex flex-col items-center">
           <HeroSlider />
         </div>
         <div className="h-screen w-screen bg-slate-500">
