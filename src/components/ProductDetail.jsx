148ea8c52c59a1168a8795e965e75b4fd2cc3329
Saksham4sure <sakshamorig123@gmail.com>
2025-11-13 21:34:21 +0545

    Update 0.7

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,10 +1,48 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { mostSoldProducts, newlyAddedProducts, } from "../context";
 import PageHeading from "./PageHeading";
 import order from "../assets/order.png"
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 const ProductDetail = () => {
   const { id } = useParams();
+  const bgRef = useRef();
+  const popUpRef = useRef();
+
+  useEffect(() => {
+    gsap.set(bgRef.current, { opacity: 0, pointerEvents: "none" });
+    gsap.set(popUpRef.current, { y: "100%", scale: 0.4 });
+  }, []);
+
+  const popUpOpen = () => {
+    gsap.to(bgRef.current, {
+      opacity: 1,
+      duration: 0.4,
+      pointerEvents: "auto",
+    });
+    gsap.to(popUpRef.current, {
+      y: 0,
+      scale: 1,
+      duration: 0.5,
+      ease: "power4.inOut",
+      delay: 0.1,
+    });
+  };
+
+  const popUpClose = () => {
+    gsap.to(bgRef.current, {
+      opacity: 0,
+      duration: 0.4,
+      pointerEvents: "none",
+    });
+    gsap.to(popUpRef.current, {
+      y: "100%",
+      scale: 0.4,
+      duration: 0.5,
+      ease: "power3.inOut",
+    });
+  };
 
   const allProducts = [...mostSoldProducts, ...newlyAddedProducts]
   const product = allProducts.find((item) => item.id === id);
@@ -18,8 +56,8 @@ const ProductDetail = () => {
 
   return (
     <>
-      <div className="h-screen w-screen bg-[#00000050] fixed z-[99999] text-center">
-        <div className="absolute h-[60%] w-[85%] md:h-[450px] md:w-[600px] bg-[#ffffff] rounded-2xl bottom-5 md:bottom-1/2 left-[50%] -translate-x-[50%] md:translate-y-1/2">
+      <div ref={bgRef} className="h-screen w-screen bg-[#00000050] fixed z-[99999] text-center">
+        <div ref={popUpRef} className="absolute h-[60%] w-[85%] md:h-[450px] md:w-[600px] bg-[#ffffff] rounded-2xl bottom-5 left-[50%] -translate-x-[50%] ">
           <div className="h-[72%] w-full flex flex-col px-4 md:px-10 items-center justify-start pt-4">
             <h1 className="font-extrabold text-2xl md:text-3xl uppercase text-[#52b345] pb-4">Place Order</h1>
             <p>Please click <span className="italic font-semibold">contact button</span> and place order with your preferred <span className="italic font-semibold text-[#52b345]">social media platform</span>.</p>
@@ -29,9 +67,9 @@ const ProductDetail = () => {
             <p className="text-sm font-light text-[#444444] cursor-default">Your order is one click away!</p>
           </div>
           <div className="h-[14%] w-full flex items-center justify-center">
-            <h1 className="bg-[#52b345] text-[#ffffff] text-xl w-[70%] md:w-[40%] h-full flex items-center justify-center rounded-full cursor-pointer font-semibold">Contact</h1>
+            <Link to="/contact" className="bg-[#52b345] text-[#ffffff] text-xl w-[70%] md:w-[40%] h-full flex items-center justify-center rounded-full cursor-pointer font-semibold">Contact</Link>
           </div>
-          <div className="h-[14%] w-full flex items-center justify-center">
+          <div onClick={popUpClose} className="h-[14%] w-full flex items-center justify-center">
             <h1 className="text-lg cursor-pointer w-full h-full flex items-center justify-center">Cancel</h1>
           </div>
         </div>
@@ -60,7 +98,7 @@ const ProductDetail = () => {
             <p className="text-[#666666]">Stock :</p>
             <p className="text-[#1cac08] underline font-light tracking-wide">{product.stock}</p>
           </div>
-          <button className="bg-[#222222] text-[#f2f2f2] flex items-center justify-center w-[150px] py-1 rounded-full">
+          <button onClick={popUpOpen} className="bg-[#222222] text-[#f2f2f2] flex items-center justify-center w-[150px] py-1 rounded-full">
             Place Order
           </button>
           <div className="w-full h-[1px] bg-[#c5c5c5] rounded-full my-2"></div>
