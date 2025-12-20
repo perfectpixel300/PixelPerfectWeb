968d6a3c6962d0e00566be433e5e6b72aa33c6b6
drxking <drxking456@gmail.com>
2025-12-20 22:50:42 +0545

    Banner

--- src/pages/NormalLayout.jsx
+++ src/pages/NormalLayout.jsx
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import ScrollToTop from "../components/ScrollToTop"
 import HoverButton from "../components/HoverButton"
+import HeroSlider from "../components/HeroSlider"
 
 const NormalLayout = ({ children }) => {
     return (
@@ -10,6 +11,9 @@ const NormalLayout = ({ children }) => {
             <HoverButton />
             <ScrollToTop />
             {children}
+            <div className="mt-10">
+                <HeroSlider />
+            </div>
 
             <Footer />
         </>
