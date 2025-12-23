5cf1e80268b1674fb73a262d6334eb241b91c159
drxking <drxking456@gmail.com>
2025-12-23 12:34:16 +0545

    Banners

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -46,17 +46,14 @@ const HeroSlider = () => {
                 className="md:absolute w-full md:h-full object-cover rounded-2xl"
               />
               <div className="md:absolute md:w-[40%] left-20 top-60 px-4 py-5 text-[#222222]">
-                <h1 className="text-2xl md:text-5xl font-semibold">{data.title}</h1>
-                <p className="md:text-sm font-light tracking-wide pt-2 pb-8 h-[135px] md:h-auto">
-                  {data.desc}
-                </p>
+
+              </div>
                 <Link
                   to={data.link}
-                  className="text-white bg-[#1cac09] text-sm md:text-xs px-4 py-2 rounded-lg duration-300 hover:bg-[#389e2a]"
+                  className="text-white absolute bottom-20 left-24 bg-[#1cac09] text-sm md:text-xs px-4 py-2 rounded-lg duration-300 hover:bg-[#389e2a]"
                 >
-                  Follow Link
+                  {data.desc}
                 </Link>
-              </div>
             </div>
           </SwiperSlide>
         ))}
